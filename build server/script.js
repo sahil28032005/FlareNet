67283d3f1458737726a198da894afd301cf9b68f
$@#il <sahilsadekar249775@gmail.com>
2024-11-15 04:08:17 +0530

    success for push to s3

--- build server/script.js
+++ build server/script.js
@@ -1,7 +1,20 @@
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
 
+// make s3cliient connetion code here
+const s3Client = new S3Client({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+});
+
+//supposed to be come from environment
+const PROJECT_ID = process.env.PROJECT_ID
 //change directory to where project is located as we are currenlty in workdir as /app
 async function init() {
     console.log("Executing script.js...");
@@ -19,8 +32,41 @@ async function init() {
         console.log('Error: ' + data.toString());
     });
 
-    processInstance.on('close', function () {
+    processInstance.on('close', async function () {
         console.log("Build completed successfully!");
+
+        //now push that builded code to s3 in the statical form
+        const distFolderPath = path.join(__dirname, 'output', 'dist');
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true }); //this will give all paths in the form of array for further use
+
+        //distfolderContents wil be array pt these values:
+        // path assets
+        // path index.html
+        // path vite.svg
+        // path assets / index - BSn - hgr8.js
+        // path assets / index - DLYYcElR.css
+
+        for (const file of distFolderContents) {
+            // console.log("path",file);
+            const filePath = path.join(distFolderPath, file);
+            if (fs.lstatSync(filePath).isDirectory()) continue; //skip file as a path as this will not occur in this case as we have each filr path with file presence
+
+            console.log('uploading', filePath);
+
+            const command = new PutObjectCommand({
+                Bucket: 'user-build-codes',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            });
+
+            //send data 
+            await s3Client.send(command);
+            console.log('uploaded', file);
+
+        }
+        console.log('done all files upload!');
+
     });
 }
 
