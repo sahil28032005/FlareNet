a9df46abd98cd72dcb209071775f18ebefc0f6d6
$@#il <sahilsadekar249775@gmail.com>
2024-11-21 15:18:47 +0530

    satarted frontend with shadcn setup

---
+++ clientReq/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from '@/components/ui/button';
+function App() {
+
+
+  return (
+    <>
+      {/* default renderer as counter */}
+      <Button variant="destructive">Destructive</Button>
+    </>
+  )
+}
+
+export default App
