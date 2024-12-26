0861229a657d3034469d33800c64d32a105cc2f8
$@#il <sahilsadekar249775@gmail.com>
2024-12-26 21:18:13 +0530

    predefine workflw skeleton ready

--- api-server-spinner/worker/webHooksWorker.js
+++ api-server-spinner/worker/webHooksWorker.js
@@ -1,12 +1,40 @@
 const { Worker } = require('bullmq');
+const triggerBuild = require('../tasks/triggerBuild');
+const runTests = require('../tasks/runTests');
+const sendNotification = require('../tasks/sendNotifications');
+
+
+//task handeler mapping
+const taskHandelers = {
+    triggerBuild,
+    runTests,
+    sendNotification
+}
 
 const webHookTaskBuilder = new Worker('webHookQueue', async (job) => {
-    const { deploymentId, projectId, gitUrl, version } = job.data;
+    const { deploymentId, workflow, context } = job.data;
 
     try {
         // Process build (you can add logic to actually build the project)
         console.log(`Building project for ${gitUrl}, version ${version}`);
 
+        //try to iterate on workflow steps and process appropriate tasks attached with it
+        console.log('workflow steps:', workflow);
+
+        for (const taskName of workflow) {
+            console.log(`processing task: ${taskName}`);
+
+            const taskHandler = taskHandelers[taskName];
+            if (!taskHandler) {
+                console.error(`No handler found for task: ${taskName}`);
+                throw new Error(`Task "${taskName}" does not have a handler`);
+            }
+
+            //execute task via providing context
+            await taskHandler(context);
+            console.log(`task ${taskName} completed`);
+
+        }
         //write same code as buildqueue worker does just done queue seperations
     }
     catch (err) {
@@ -23,8 +51,10 @@ const webHookTaskBuilder = new Worker('webHookQueue', async (job) => {
 // Listen for job completion or failure
 webHookTaskBuilder.on('completed', (job) => {
     console.log(`Build job ${job.id} completed successfully.`);
-  });
-  
-  webHookTaskBuilder.on('failed', (job, err) => {
+});
+
+webHookTaskBuilder.on('failed', (job, err) => {
     console.error(`Build job ${job.id} failed with error: ${err.message}`);
-  });
+});
+
+module.exports = webHookTaskBuilder;
